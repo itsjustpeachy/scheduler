fb8cb997df5ebcf9e61eff01480ffde2a0fcdc4d
peachy <trevsal09@gmail.com>
2021-11-07 13:55:21 -0800

    elements for save button and icon created, todo: local storage saver

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,31 +1,45 @@
-var currentTime = moment()
-var currentMin = moment().minutes()
-var timeSuffix = ":00am"
+var userContainer = document.getElementById('container')
 
+var currentTime = moment()
+var currentHour = moment().hour()
+var currentMin = moment().minutes()
+
+var timeSuffix = ":00am"
 var timeLabel = 9
 
-var userContainer = document.getElementById('container')
-//Forms dynamic html elements on the page
+//Forms dynamic html elements on the page for time, text box, and save btn
 function generateTimeBlocks(iterations) {
 
+    //for loop that iterates for desired amount (9)
     if(!iterations) {
         iterations = 1
     }
     for (var i = 0; i < iterations; i++) {
 
         time = timeLabel + timeSuffix
-
-        $Block = $("<div>").addClass("row py-1");
+        
+        //creates the row div to hold time, text, & save btn
+        $rowDiv = $("<div>").addClass("row py-1")
     
-        $TimeText = $("<h5>").addClass("text-center").text(time)
-        $TimeDiv = $("<div>").addClass("col-2 py-3 hour align-middle").append($TimeText)
-        $TextDiv = $("<textarea>").addClass("col-8 py-3 overflow-auto").text("Enter Event")
+        //generates and appends elements to their parents elements
+        $TimeText = $("<h5>").addClass("time-block").text(time)
+        $TimeDiv = $("<div>").addClass("col-2 py-3 hour").append($TimeText)
+        $TextDiv = $("<textarea>").addClass("col-8 py-3 textarea description").text("")
 
-        $Block.append($TimeDiv, $TextDiv)
-    
-        $("#container").append($Block)
+        //save button html
+        $saveBtnIcon = $("<img>").attr('src','./assets/images/Save-Icon.png').addClass("saveBtnIcon")
+        $saveBtnDiv = $("<div>").addClass("col-1 py-3 saveBtn").append($saveBtnIcon)
+
+        //todo: set bg color
+
+        //appends formed blocks to the main div and container on page
+        $rowDiv.append($TimeDiv, $TextDiv, $saveBtnDiv)
+        $("#container").append($rowDiv)
+
+        //calls timeLabelSet to dynamically change the suffix from am to pm
         timeLabelSet()
     }}
+
 //Sets the timeLabel variable to display a 9-5 work day
 function timeLabelSet() {
     if(timeLabel == 12){
@@ -34,7 +48,7 @@ function timeLabelSet() {
         timeSuffix = ":00pm"
         timeLabel++
     } else {
-        timeLabel++;
+        timeLabel++
     }
 }
 
